46b91b79b5437afdd6f2e8fa29faa87943662490
Vanja Komadinovic <vanjakom@gmail.com>
2020-08-28 13:48:29 +0200

    cache cleanup

--- src/clj_common/cache.clj
+++ src/clj_common/cache.clj
@@ -51,20 +51,19 @@
          (io/write output-stream (value-serialize-fn value)))))))
 
 
-(comment
-  (let [cache (create-local-fs-cache
-                {
-                  :cache-path (path/string->path "/tmp/cache")
-                  :key-fn clj-common.hash/string->md5-string
-                  :value-serialize-fn clj-common.json/serialize
-                  :value-deserialize-fn clj-common.json/deserialize})]
+#_(let [cache (create-local-fs-cache
+             {
+              :cache-path (path/string->path "/tmp/cache")
+              :key-fn clj-common.hash/string->md5-string
+              :value-serialize-fn clj-common.json/serialize
+              :value-deserialize-fn clj-common.json/deserialize})]
 
-    (cache "test1")
+  (cache "test1")
 
-    (cache "test1" {:a 10})
+  (cache "test1" {:a 10})
 
 
-    (cache "test1"))
+  (cache "test1"))
 
-  (clj-common.json/deserialize
-    (clj-common.json/serialize {:a 10 :b (.getBytes "test")})))
+#_(clj-common.json/deserialize
+ (clj-common.json/serialize {:a 10 :b (.getBytes "test")}))
