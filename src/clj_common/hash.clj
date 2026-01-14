0f544f7ca324e8bfa55eb1dfaca26720c90cd9db
Vanja Komadinovic <vanjakom@gmail.com>
2026-01-14 11:56:13 +0100

    hash, md5 streaming

--- src/clj_common/hash.clj
+++ src/clj_common/hash.clj
@@ -9,6 +9,17 @@
          (.digest md5-digest bytes))
     16))
 
+(defn md5-input-stream ^bytes
+  [^java.io.InputStream is]
+  (let [md (java.security.MessageDigest/getInstance "MD5")
+        buffer (byte-array 8192)]
+    (loop []
+      (let [n (.read is buffer)]
+        (when (pos? n)
+          (.update md buffer 0 n)
+          (recur))))
+    (.digest md)))
+
 (defn md5-string [string]
   (md5-bytes (.getBytes string)))
 
